114249a92a447a8ff652b09fa4391d2ebf1b2b15
Richard Liu <richardadesigner@gmail.com>
2026-01-27 18:40:57 -0800

    update

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ import { HASH } from "./hash.js"
 document.getElementById("InputForm").addEventListener("submit", function (e) {
     e.preventDefault();
     const inputs = new FormData(this);
-    console.log(inputs.get("type"))
     HASH(inputs.get("Input"), inputs.get("type"))
         .then(function (output) {
             document.getElementById("display").innerText = output;
